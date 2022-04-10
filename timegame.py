dfbf76fd6bb2ee41fb26faf098030a05b0edda66
CmdLari <studio.lp@gmx.de>
2022-04-10 19:36:27 +0200

    Revert "Just a test"
    This reverts commit bb9fb959f7f8ce6c7375228ad42cae3b8697aea7.

--- timegame.py
+++ timegame.py
@@ -10,8 +10,6 @@ from background import Background
 from player import LEFT, RIGHT, UP, DOWN, Player
 from level1 import Level1
 
-# This is a change
-
 class Timegame:
     '''Manages the game'''
 
