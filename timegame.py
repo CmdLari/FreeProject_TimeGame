502e33ca49254417fffa9e299488f298fcee93fb
CmdLari <studio.lp@gmx.de>
2022-04-11 15:56:18 +0200

    New Game now resets all data and continue will do just that

--- timegame.py
+++ timegame.py
@@ -40,6 +40,21 @@ class Timegame:
         self.mov_y = json_data["mov_y"]
         self.map_x = json_data["map_x"]
         self.map_y = json_data["map_y"]            
+
+        # Player Stats
+        player_state_file = open("playerstate.json")
+        json_data = json.load(player_state_file)
+        
+        self.player_lvl = json_data["player_level"]
+        self.sanity = json_data["player_sanity"]
+        self.love = json_data["player_love"]
+        self.rationality = json_data["player_rationality"]
+        self.health = json_data["player_health"]
+
+        # Music
+        if self.player_lvl == 1:
+            self.ingame_music = pygame.mixer.Sound('_MUS/timegame_lvl1.mp3')
+
               
     def run_game(self):
         '''Main loop for the game'''
@@ -85,12 +100,45 @@ class Timegame:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
             elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                # New Game
                 if self.ng_rect.collidepoint(pygame.mouse.get_pos()):
+                    
+                    # Reset all stats
+                    self.player_lvl = 1
+                    Utils.write_to_playerstate("player_level", self.player_lvl, self.playerstats_file)
+                    self.sanity = 50
+                    Utils.write_to_playerstate("player_sanity", self.sanity, self.playerstats_file)
+                    self.love = 0
+                    Utils.write_to_playerstate("player_love", self.love, self.playerstats_file)
+                    self.rationality = 0
+                    Utils.write_to_playerstate("player_rationality", self.rationality, self.playerstats_file)
+                    self.health = 20
+                    Utils.write_to_playerstate("player_health", self.health, self.playerstats_file)
+                    
+                    self.mov_x = 0
+                    Utils.write_to_playerstate("mov_x", self.mov_x, self.playerstats_file)
+                    self.mov_y = 0
+                    Utils.write_to_playerstate("mov_y", self.mov_y, self.playerstats_file)                    
+                    self.map_x = 0
+                    Utils.write_to_playerstate("map_x", self.map_x, self.playerstats_file)                    
+                    self.map_y = 0
+                    Utils.write_to_playerstate("map_y", self.map_y, self.playerstats_file)     
+
+                    pygame.mixer.Sound.stop(self.menusound)
                     self.game_run_active = True
-                    if self.player.playerlvl ==1:
-                        self.lvl1sound = pygame.mixer.Sound('_MUS/timegame_lvl1.mp3')
-                        pygame.mixer.Sound.play(self.lvl1sound, loops=-1)
+                    pygame.mixer.Sound.play(self.ingame_music, loops=-1)                        
+
+                
+                # Continue
+                elif self.cont_rect.collidepoint(pygame.mouse.get_pos()):
+                    pygame.mixer.Sound.stop(self.menusound)                    
+                    self.game_run_active = True
+                    pygame.mixer.Sound.play(self.ingame_music, loops=-1)                        
+                
+                # Quit
                 elif self.quit_rect.collidepoint(pygame.mouse.get_pos()):
                     sys.exit()
 
@@ -114,38 +162,43 @@ class Timegame:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
 
-            # Movement #5
+            # Movement 
+            
+            # --Left
             elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 self.player.direction = LEFT
                 if self.bg.bg_rect_x <= self.bg.screen.get_rect().x:
-                    self.mov_x += 5
+                    self.mov_x += 3
                     Utils.write_to_playerstate("mov_x", self.mov_x, self.playerstats_file)
                     self.mov_x = Utils.read_from_playerstate("mov_x", self.playerstats_file)
                     self.map_x = self.bg.bg.get_rect().x+self.mov_x
                     Utils.write_to_playerstate("map_x", self.map_x, self.playerstats_file)
-
+            
+            # --Right
             elif event.key == pygame.K_d or event.key == pygame.K_RIGHT: 
                 self.player.direction = RIGHT
                 if self.bg.bg_rect_x >= self.bg.screen.get_rect().x - self.bg.screen.get_rect().width*3:
-                    self.mov_x -= 5
+                    self.mov_x -= 3
                     Utils.write_to_playerstate("mov_x", self.mov_x, self.playerstats_file)
                     self.mov_x = Utils.read_from_playerstate("mov_x", self.playerstats_file)
                     self.map_x = self.bg.bg.get_rect().x+self.mov_x
                     Utils.write_to_playerstate("map_x", self.map_x, self.playerstats_file)
 
+            # --Up
             elif event.key == pygame.K_w or event.key == pygame.K_UP:
                 self.player.direction = UP
                 if self.bg.bg_rect_y <= self.bg.screen.get_rect().y:
-                    self.mov_y += 5
+                    self.mov_y += 3
                     Utils.write_to_playerstate("mov_y", self.mov_y, self.playerstats_file)
                     self.mov_y = Utils.read_from_playerstate("mov_y", self.playerstats_file)
                     self.map_y = self.bg.bg.get_rect().y+self.mov_y
                     Utils.write_to_playerstate("map_y", self.map_y, self.playerstats_file)
-
+            
+            # --Down
             elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 self.player.direction = DOWN
                 if self.bg.bg_rect_y >= self.bg.screen.get_rect().y - self.bg.screen.get_rect().height*3:
-                    self.mov_y -= 5
+                    self.mov_y -= 3
                     Utils.write_to_playerstate("mov_y", self.mov_y, self.playerstats_file)
                     self.mov_y = Utils.read_from_playerstate("mov_y", self.playerstats_file)
                     self.map_y = self.bg.bg.get_rect().y+self.mov_y
@@ -153,9 +206,8 @@ class Timegame:
 
             # Return to menu
             elif event.key == pygame.K_r:
+                pygame.mixer.Sound.stop(self.ingame_music)
                 self.game_run_active = False
-                pygame.mixer.Sound.stop(self.lvl1sound)
-                self.menusound = pygame.mixer.Sound('_MUS/timegame_menu.mp3')
                 pygame.mixer.Sound.play(self.menusound, loops=-1)
 
             # Info-Screen
@@ -173,6 +225,7 @@ class Timegame:
         self.cont_button = pygame.image.load("_IMGS/continue.png")
         self.cont_rect_w = self.cont_button.get_rect().width
         self.cont_rect_h = self.cont_button.get_rect().height
+        self.cont_rect = pygame.Rect((self.bg.width//2-self.cont_rect_w//2, self.bg.height//2-self.cont_rect_h//2 - self.cont_rect_h//2),(self.cont_rect_w, self.cont_rect_h))
         self.bg.screen.blit(self.cont_button, (self.bg.width//2-self.cont_rect_w//2, self.bg.height//2-self.cont_rect_h//2))
 
         # New Game
@@ -196,14 +249,14 @@ class Timegame:
 
     def check_player_lvl(self):
         '''Checks for player level and sets map'''
-        if self.player.playerlvl == 1:
+        if self.player_lvl == 1:
             self.bglvl1 = pygame.image.load('_IMGS/_lvl1/map_lvl1.png')
             self.bg.bg = self.bglvl1
             pygame.mixer.Sound.stop(self.menusound)
 
     def draw_lvl(self):
         '''Initializes lvl assets'''
-        if self.player.playerlvl == 1:
+        if self.player_lvl == 1:
             self.lvl1.backgroundlvl1_assets()
 
 if __name__ == '__main__':
